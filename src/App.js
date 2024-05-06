c7826047e50d4e2a637917f70c61ea2b18cbbcb1
swaroopn96 <swaroopn96@gmail.com>
2024-05-06 14:23:20 +0530

    Updated readme and added different ways to call event

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ export default function App() {
   const handleClick = () => {
     console.log("Button was clicked");
   };
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={function () {
+  //         console.log("Button was clicked");
+  //       }}
+  //     >
+  //       Add Animal
+  //     </button>
+  //   </div>
+  // );
+
   return (
     <div>
-      <button onClick={handleClick}>Add Animal</button>
+      <button
+        onClick={() => {
+          console.log("Button was clicked");
+        }}
+      >
+        Add Animal
+      </button>
     </div>
   );
 }
