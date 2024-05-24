4968bcd06c8e5cd944347df7d9afb2e7eb9342df
swaroopn96 <swaroopn96@gmail.com>
2024-05-24 21:25:36 +0530

    Array destructuring

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import { useState } from "react";
 export default function App() {
-  const [count, setCount] = useState(0);
+  const stateConfig = useState(0);
 
-  const handleClick = () => {
-    console.log(count);
-    setCount(count + 1);
-  };
+  function makeArray() {
+    return [0, 1, 2, 4, 6];
+  }
 
-  return (
-    <div>
-      <button onClick={handleClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
-    </div>
-  );
+  //Normal way of assigning
+  const myArray = makeArray();
+  const firstElement = myArray[0];
+  const secondElement = myArray[1];
+
+  console.log(firstElement, secondElement);
+
+  //Using simple array destructuring
+  const [fElement, sElement] = makeArray();
+  console.log(fElement, sElement);
 }
+
+//This is how useState Works internally
+function useState(defaultValue) {
+  return {
+    yourState: defaultValue,
+    yourSetter: () => {},
+  };
+}
+
+//Internal workflow
+// let render = -1;
+// const states = {};
+
+// const useState = (value) => {
+//   const id = ++render;
+
+//   const setValue = (newValue) => {
+//     value = newValue;
+//     manualRerender();
+//   };
+//   const state = [value, setValue];
+//   states[id] = state;
+//   console.log(states);
+//   return state;
+// };
