975e2f3de73d8be479c55b2971cf24f5b630183f
swaroopn96 <swaroopn96@gmail.com>
2024-05-25 13:24:09 +0530

    Implemented code to show added random animals to screen

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
+import { useState } from "react";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+console.log(getRandomAnimal());
+
 export default function App() {
-  const stateConfig = useState(0);
+  const [animals, setAnimals] = useState([]);
 
-  function makeArray() {
-    return [0, 1, 2, 4, 6];
-  }
+  const handleClick = () => {
+    //Its creating a new array and takes all existing elements from animals and add random animal at end
+    setAnimals([...animals, getRandomAnimal()]);
 
-  //Normal way of assigning
-  const myArray = makeArray();
-  const firstElement = myArray[0];
-  const secondElement = myArray[1];
-
-  console.log(firstElement, secondElement);
-
-  //Using simple array destructuring
-  const [fElement, sElement] = makeArray();
-  console.log(fElement, sElement);
-}
-
-//This is how useState Works internally
-function useState(defaultValue) {
-  return {
-    yourState: defaultValue,
-    yourSetter: () => {},
+    //We could have done like below but it modifies peice of state
+    //animals.push(getRandomAnimal());
   };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
+    </div>
+  );
 }
-
-//Internal workflow
-// let render = -1;
-// const states = {};
-
-// const useState = (value) => {
-//   const id = ++render;
-
-//   const setValue = (newValue) => {
-//     value = newValue;
-//     manualRerender();
-//   };
-//   const state = [value, setValue];
-//   states[id] = state;
-//   console.log(states);
-//   return state;
-// };
